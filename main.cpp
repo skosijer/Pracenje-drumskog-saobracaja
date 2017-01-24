e37e8e8de9695ff5b07e9e0ba3e8dd1994e0a90f
Stevan Kosijer <stkosijer@gmail.com>
2017-01-24 18:22:11 +0100

    Ucitavanje videa i pustanje videa.

---
+++ main.cpp
@@ -0,0 +1,61 @@
+﻿#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+int main(void) {
+
+	cv::VideoCapture capVideo;
+
+	cv::Mat imgFrame;
+
+	capVideo.open("SnimakAutoputa.mp4");
+
+	//greska prilikom ucitavanja videaa
+	if (!capVideo.isOpened()) {                                                
+		std::cout << "\nError reading video file" << std::endl << std::endl;   
+		_getch();                   
+		return(0);                                                              
+	}
+
+	//proveriti dodatno
+	if (capVideo.get(CV_CAP_PROP_FRAME_COUNT) < 1) {
+		std::cout << "\nError: video file must have at least one frame";
+		_getch();
+		return(0);
+	}
+
+	//ucitavanje prvog imageFrame-a
+	capVideo.read(imgFrame);
+
+	char chCheckForEscKey = 0;
+
+	while (capVideo.isOpened() && chCheckForEscKey != 27) {
+
+		cv::imshow("Pracenje drumskog saobracaja", imgFrame);
+
+		//provera da li ima jos frame-ova za citanje
+		if ((capVideo.get(CV_CAP_PROP_POS_FRAMES) + 1) < capVideo.get(CV_CAP_PROP_FRAME_COUNT)) {       
+			capVideo.read(imgFrame);                           
+		}
+		else {                                                 
+			std::cout << "End of video\n";                     
+			break;                                             
+		}
+
+		chCheckForEscKey = cv::waitKey(1);      
+
+	}
+
+	if (chCheckForEscKey != 27) {             
+		cv::waitKey(0);                     
+	}
+
+	return 0;
+}
+
+
